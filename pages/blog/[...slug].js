55eb66d85767fc1025df24bdb357fd9763be7f3c
Sachin Yadav <88329785+designed2code@users.noreply.github.com>
2021-12-08 17:06:27 +0530

    Delete pages/blog directory

--- pages/blog/[...slug].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router"
-const BlogPostsPage = () => {
-    const router = useRouter()
-    console.log(router.query);
-    return(
-        <h1>The Blog Posts</h1>
-    )
-}
-export default BlogPostsPage
